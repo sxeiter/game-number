b6a4427034634b491a495b460f463222aaaa402b
Павел Бакшеев <sxeiter@mail.ru>
2023-10-11 15:29:32 +0500

    исправил дз, добавил цикл while

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-let number = parseInt(Math.random() * 100);
-guess(number);
-
-function guess(num) {
-    let answer = parseInt(prompt('Угадай число от 1 до 100'));
-    if (answer > num) {
-        alert('Ваше число больше загаданного');
-    } else if(answer < num) {
-        alert('Ваче число меньше загаданного');
-    } else if (answer == num) {
-        alert('Вы угадали!');
-        return;
-    } else {
-        alert('Необходимо ввести число!')
+function guessNumber() {
+    const secretNumber = Math.floor(Math.random() * 100) + 1;
+    let guess;
+  
+    while (guess !== secretNumber) {
+      guess = prompt("Введите число от 1 до 100 (или 'Отмена' для выхода):");
+  
+      if (guess === null) {
+        alert("Игра завершена.");
+        break;
+      }
+  
+      guess = parseInt(guess);
+  
+      if (isNaN(guess)) {
+        alert("Введи число!");
+      } else if (guess < secretNumber) {
+        alert("Больше!");
+      } else if (guess > secretNumber) {
+        alert("Меньше!");
+      } else {
+        alert("Правильно!");
+      }
     }
-    guess(num);
-}
-guess(number);
+  }
+  
+  guessNumber();
