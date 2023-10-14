ceeef91de56e404deaa3092e5f2cb2bf44e82719
Павел Бакшеев <sxeiter@mail.ru>
2023-10-14 14:35:46 +0500

    дз 11 часть 1

---
+++ task02.js
@@ -0,0 +1,14 @@
+function recursiveFunction(arr) {
+    if (arr.reduce((sum, num) => sum + num, 0) >= 50) {
+      return arr;
+    } else {
+      const num = Math.floor(Math.random() * 11);
+      arr.push(num);
+      return recursiveFunction(arr);
+    }
+  }
+  
+  // Пример использования
+  const myArray = [];
+  const result = recursiveFunction(myArray);
+  console.log(result);
